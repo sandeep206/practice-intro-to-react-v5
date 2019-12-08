82593884d5a327e3e4badd1f56f220018ba949b5
Sasidharan, Sandeep (ITCDCC) - KLM <Sandeep.Sasidharan@klm.com>
2019-12-08 20:25:31 +0100

    search-params,pets,results components added

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
+const Pet = ({ name, animal, breed, media, location, id }) => {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero - media[0].small;
+  }
+
+  return (
+    <a href={`/defaults/${id}`}  className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
+  )
+
 };
 
 export default Pet;
